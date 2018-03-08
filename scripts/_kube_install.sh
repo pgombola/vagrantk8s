49336dca3c9d2846d9e7d7eaaff6c0567ebdb4be
Patrick Gombola <pgumby@gmail.com>
2018-03-08 12:51:40 -0500

    Add install of aufs-tools and add -q to kube install

--- scripts/_kube_install.sh
+++ scripts/_kube_install.sh
@@ -4,13 +4,13 @@ set -eou pipefail
 IFS=$'\n\t'
 
 apt-get update \
-  && apt-get install -qy docker.io
-  
+  && apt-get install -qy aufs-tools docker.io
+
 apt-get update && apt-get install -y apt-transport-https \
   && curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-  
+
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-  
+
 apt-get update \
-  && apt-get install -y conntrack kubelet kubeadm kubernetes-cni
+  && apt-get install -qy conntrack kubelet kubeadm kubernetes-cni
 
