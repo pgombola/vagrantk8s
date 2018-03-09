1f246f1e4d65290a6fdb7707155718162cd439f2
Patrick Gombola <pgombola@cleo.com>
2018-03-09 11:58:18 -0500

    Getting started on an app to connect to cockroachdb

--- src/cmd/dbtest/main.go
+++ src/cmd/dbtest/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "log"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	// Import postgres driver.
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	log.Println("App started")
+	log.Println("starting app")
+
+	url := fmt.Sprintf("postgresql://root@cockroachdb-public:26257")
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var dbErr error
+	maxAttempts := 20
+	for attempts := 1; attempts <= maxAttempts; attempts++ {
+		dbErr = db.Ping()
+		if dbErr == nil {
+			break
+		}
+		log.Println(dbErr)
+		time.Sleep(time.Duration(attempts) * time.Second)
+	}
+	if dbErr != nil {
+		log.Fatal(dbErr)
+	}
+
+	log.Println("app started")
 }
